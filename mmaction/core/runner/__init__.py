d04e01cf6da765f67b499350cdb9b3d2ec82449e
sli057 <sli057@ucr.edu>
2021-10-23 23:23:28 -0700

    remove files

--- mmaction/core/runner/__init__.py
+++ mmaction/core/runner/__init__.py
@@ -1,6 +1,7 @@
 from .omnisource_runner import OmniSourceDistSamplerSeedHook, OmniSourceRunner
-from .pair_runner import PairRunner
-from .hooks import Alternative_Train_GeneratorOptimizerHook
+#from .pair_runner import PairRunner
+#from .hooks import Alternative_Train_GeneratorOptimizerHook
 
-__all__ = ['OmniSourceRunner', 'OmniSourceDistSamplerSeedHook','PairRunner',
-           'Alternative_Train_GeneratorOptimizerHook']
+__all__ = ['OmniSourceRunner', 'OmniSourceDistSamplerSeedHook'#,'PairRunner',
+           #'Alternative_Train_GeneratorOptimizerHook']
+            ]
