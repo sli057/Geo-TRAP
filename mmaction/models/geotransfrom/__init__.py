1a6e8e113e9552d70fd822a60d50feeb03ee4772
sli057 <sli057@ucr.edu>
2021-10-23 22:38:54 -0700

    delete files

--- mmaction/models/geotransfrom/__init__.py
+++ mmaction/models/geotransfrom/__init__.py
@@ -1,8 +1,1 @@
-from .MP_model import MPGDTarget, MPGDUnTarget, MPGDUnTargetStatic
-from .DecomposeMP_model import (DecomposeG, DecomposeTargetedG,
-                                DecomposeGDUnTarget, DecomposeGDTarget, DecomposeGDOneTarget)
-from .flicker_model import FlickerGeneratorUnTarget, FlickerGDUnTarget
-__all__ = ['MPGDTarget', 'MPGDUnTarget', 'MPGDUnTargetStatic',
-           'DecomposeG', 'DecomposeTargetedG',
-           'DecomposeGDUnTarget', 'DecomposeGDOneTarget', 'DecomposeGDTarget',
-           'FlickerGeneratorUnTarget', 'FlickerGDUnTarget']
+
