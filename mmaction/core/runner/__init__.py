3ffa4d3e04c4309a97073dc2c0320fdffdd50c0e
sli057 <sli057@ucr.edu>
2021-10-23 23:21:23 -0700

    change __init__

--- mmaction/core/runner/__init__.py
+++ mmaction/core/runner/__init__.py
@@ -1,6 +1,6 @@
 from .omnisource_runner import OmniSourceDistSamplerSeedHook, OmniSourceRunner
 from .pair_runner import PairRunner
-from .hooks import GeneratorOptimizerHook, Alternative_Train_GeneratorOptimizerHook
+from .hooks import Alternative_Train_GeneratorOptimizerHook
 
-__all__ = ['OmniSourceRunner', 'OmniSourceDistSamplerSeedHook','PairRunner', 'GeneratorOptimizerHook',
+__all__ = ['OmniSourceRunner', 'OmniSourceDistSamplerSeedHook','PairRunner',
            'Alternative_Train_GeneratorOptimizerHook']
